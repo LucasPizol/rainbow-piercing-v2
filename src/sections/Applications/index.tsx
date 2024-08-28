8ba7a5b6205cd71a5fea977505b6b688c650665e
aluno <aluno@UNIFEOB-ACAD.LOCAL>
2024-08-27 21:02:47 -0300

    feat: adding images

--- src/sections/Applications/index.tsx
+++ src/sections/Applications/index.tsx
@@ -1,27 +1,58 @@
-import { TypographySubtitle } from "@/components/typography";
+import { TypographyBody, TypographySubtitle } from "@/components/typography";
 import styles from "./styles.module.css";
 import { Button } from "@/components/button";
 
-const applications = [
-  "https://via.placeholder.com/150",
-  "https://via.placeholder.com/150",
-  "https://via.placeholder.com/150",
-  "https://via.placeholder.com/150",
-  "https://via.placeholder.com/150",
-  "https://via.placeholder.com/150",
-  "https://via.placeholder.com/150",
-  "https://via.placeholder.com/150",
-];
+import aplicacao01 from "@/assets/aplicacoes/aplicacao-01.jpeg";
+import aplicacao02 from "@/assets/aplicacoes/aplicacao-02.jpeg";
+import aplicacao03 from "@/assets/aplicacoes/aplicacao-03.jpeg";
+import aplicacao04 from "@/assets/aplicacoes/aplicacao-04.jpeg";
+import aplicacao05 from "@/assets/aplicacoes/aplicacao-05.jpeg";
+import aplicacao06 from "@/assets/aplicacoes/aplicacao-06.jpeg";
+import aplicacao07 from "@/assets/aplicacoes/aplicacao-07.jpeg";
+import aplicacao08 from "@/assets/aplicacoes/aplicacao-08.jpeg";
+import aplicacao09 from "@/assets/aplicacoes/aplicacao-09.jpeg";
+import aplicacao10 from "@/assets/aplicacoes/aplicacao-10.jpeg";
+import aplicacao11 from "@/assets/aplicacoes/aplicacao-11.jpeg";
+import Image from "next/image";
 
+const images = [
+  aplicacao01,
+  aplicacao02,
+  aplicacao03,
+  aplicacao04,
+  aplicacao05,
+  aplicacao06,
+  aplicacao07,
+  aplicacao08,
+  aplicacao09,
+  aplicacao10,
+];
 export const Applications = () => {
   return (
     <section className={styles.applicationsSection}>
       <div className={styles.applicationsSectionWrapper}>
         <TypographySubtitle>APLICAÇÕES</TypographySubtitle>
+        <TypographyBody
+          style={{
+            textAlign: "center",
+          }}
+        >
+          A satisfação de nossos(as) clientes é a parte mais importante do
+          negócio!
+        </TypographyBody>
 
         <div className={styles.applicatinsCardsWrapper}>
-          {applications.map((_, index) => (
-            <div className={styles.card} key={index} />
+          {images.map((image, index) => (
+            <div className={styles.card} key={index}>
+              <Image
+                width={250}
+                height={250}
+                src={image}
+                alt={`Imagem produto piercing ${index}`}
+                placeholder="blur"
+                blurDataURL={image.src}
+              />
+            </div>
           ))}
         </div>
       </div>
--- src/sections/Catalog/index.tsx
+++ src/sections/Catalog/index.tsx
@@ -1,6 +1,34 @@
 import { Button } from "@/components/button";
 import styles from "./styles.module.css";
 import { TypographyBody, TypographyTitle } from "@/components/typography";
+import produto01 from "@/assets/produtos/produto-01.jpeg";
+import produto02 from "@/assets/produtos/produto-02.jpeg";
+import produto03 from "@/assets/produtos/produto-03.jpeg";
+import produto04 from "@/assets/produtos/produto-04.jpeg";
+import produto05 from "@/assets/produtos/produto-05.jpeg";
+import produto06 from "@/assets/produtos/produto-06.jpeg";
+import produto07 from "@/assets/produtos/produto-07.jpeg";
+import produto08 from "@/assets/produtos/produto-08.jpeg";
+import produto09 from "@/assets/produtos/produto-09.jpeg";
+import produto10 from "@/assets/produtos/produto-10.jpeg";
+import produto11 from "@/assets/produtos/produto-11.jpeg";
+import produto12 from "@/assets/produtos/produto-12.jpeg";
+import Image from "next/image";
+
+const images = [
+  produto01,
+  produto02,
+  produto03,
+  produto04,
+  produto05,
+  produto06,
+  produto07,
+  produto08,
+  produto09,
+  produto10,
+  produto11,
+  produto12,
+];
 
 export const Catalog = () => {
   return (
@@ -11,6 +39,22 @@ export const Catalog = () => {
           Conheça nossos produtos e escolha o que mais combina com você. Baixe
           nosso catálogo e veja todas as opções disponíveis.
         </TypographyBody>
+
+        <div className={styles.products}>
+          {images.map((image, index) => (
+            <div key={index} style={{ width: "100%", aspectRatio: "1/1" }}>
+              <Image
+                width={250}
+                height={250}
+                src={image}
+                alt={`Imagem produto piercing ${index}`}
+                placeholder="blur"
+                blurDataURL={image.src}
+              />
+            </div>
+          ))}
+        </div>
+
         <Button>BAIXAR CATÁLOGO</Button>
       </div>
     </section>
