6a8d8bc3ef63840c5c88117755cdeb97b2d7f1fd
Lucas Pizol Ferreira <lucaspizolfe@gmail.com>
2024-10-20 10:18:30 -0300

    feat: analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,11 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import Head from "next/head";
 
 export const metadata: Metadata = {
   title: "Rainbow Piercing",
-  description: "Os melhores piercing de Poços de Caldas e região. Adquira já o seu!",
+  description:
+    "Os melhores piercing de Poços de Caldas e região. Adquira já o seu!",
 };
 
 export default function RootLayout({
@@ -13,6 +15,23 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt">
+      <Head>
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-TKVRS8F6MW"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `<script>
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'G-MJLWJ7KHBB');
+</script>`,
+          }}
+        ></script>
+      </Head>
       <body>{children}</body>
     </html>
   );
